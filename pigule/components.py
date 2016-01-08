9a5688a2509a3729fcb59f29eb125bb1cd6c3bdd
Marien Fressinaud <dev@marienfressinaud.fr>
2016-01-08 21:53:32 +0100

    Provide the Age component

--- pigule/components.py
+++ pigule/components.py
@@ -1,6 +1,16 @@
 from pytity.component import Component
 
 
+class Age(Component):
+    """Define the age of an entity
+    """
+    def __init__(self):
+        Component.__init__(self, 0)
+
+    def inc(self, value=1):
+        self.value += value
+
+
 class Clonable(Component):
     """Define if an entity can be cloned or not
     """
--- tests/test_archetypes.py
+++ tests/test_archetypes.py
@@ -1,11 +1,12 @@
 import pigule.archetypes as archetypes
-from pigule.components import Clonable
+from pigule.components import Age, Clonable
 
 
 def test_create_master_cell(manager):
     master_cell = archetypes.create_master_cell(manager)
 
     assert master_cell.get_component(Clonable) is not None
+    assert master_cell.get_component(Age) is not None
 
 
 def test_create_cells(manager):
@@ -13,3 +14,4 @@ def test_create_cells(manager):
 
     assert len(list(manager.entities())) == 2
     assert len(list(manager.entities_by_type(Clonable))) == 0
+    assert len(list(manager.entities_by_type(Age))) == 2
