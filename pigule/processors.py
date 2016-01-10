75cd9b7cb71f3f22b57a5b7db281be5ad94ab7af
Marien Fressinaud <dev@marienfressinaud.fr>
2016-01-10 11:26:35 +0100

    Add comments to processors

--- pigule/processors.py
+++ pigule/processors.py
@@ -5,12 +5,16 @@ from pigule.components import Age, Clonable, Mortality
 
 
 class Reproduction(Processor):
+    """Manage reproduction of clonable cells
+    """
     def update(self, delta):
         clonable_entities = list(self.manager.entities_by_type(Clonable))
         archetypes.create_cells(self.manager, len(clonable_entities))
 
 
 class Time(EntityProcessor):
+    """Manage time-based operations such as cell age or mortality
+    """
     def __init__(self):
         EntityProcessor.__init__(self)
         self.needed = [Age]
